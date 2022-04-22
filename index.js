84e7bb486e87c4d9351212941c78fd96190b879e
khaldar <adnan-5-@outlook.com>
2022-04-22 16:12:11 +0300

    completed map method

--- index.js
+++ index.js
@@ -10,7 +10,45 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
-
-const titleCased = () => {
-  return tutorials
+// USE FOR OF LOOP
+function titleCased() {
+  const answer = tutorials.toString().split(",").map(char => char[0].toUpperCase() + char.substring(1)).toString().split(" ").map(char => char[0].toUpperCase() + char.substring(1)).join(" ").split(",");
+  /* for (const tutorial of tutorials) {
+    return tutorial.split(' ').map(function(word) {
+      return (word.charAt(0).toUpperCase() + word.slice(1));
+    }).join(' ')
+  } */
+  
+  /*
+  const lowerCase = tutorials.toLowerCase();
+  const splitter = lowerCase.split(' ');
+  const mapping = splitter.map(function(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  })
+  const answer = mapping.join(' ');
+  // const uppers = ["Oo", "Api", "Jsonp"];
+  // for (let i = 0; i < uppers.length; i++) {
+  //   answer = answer.replace(new RegExp('\\b' + uppers[i] + '\\b', 'g'), 
+  //     uppers[i].toUpperCase());
+  // }
+  for (const word of tutorials) {
+    if (word === "Oo") {
+      return "OO";
+    } else if (word === "Api") {
+      return "API";
+    } else if (word === "Jsonp") {
+      return "JSNOP";
+    } else if (word === "Nan") {
+      return "NaN"
+    }
+  }
+  // const uppers = ["Oo", "Api", "Jsonp"];
+  // uppers = ["OO", "API", "JSONP"]
+  // const consts = ["Nan"]
+  // consts = ["NaN"]
+  return answer */
+  return answer
 }
+const myFunc = titleCased()
+console.log(myFunc)
+// console.log(titleCased())
